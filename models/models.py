0a5ef5562454895dd4ca3f53d7a81f5e822add62
playoung2818 <jameschen2818@gmail.com>
2025-07-09 16:17:09 -0500

    Initial commit

--- models/models.py
+++ models/models.py
@@ -5,10 +5,11 @@ db = SQLAlchemy()
 class ReceivingLog(db.Model):
     __tablename__ = 'receiving_log'
     serial_number = db.Column(db.String(255), primary_key=True)
-    entry_date = db.Column(db.Date)
-    invoice_number = db.Column(db.String(255))
-    box_number = db.Column(db.String(255))
-    pod_number = db.Column(db.String(255))
-    part_number = db.Column(db.String(255))
-    quantity = db.Column(db.Float)
+    entry_date    = db.Column(db.Date)
+    invoice_number= db.Column(db.String(255))
+    box_number    = db.Column(db.String(255))
+    pod_number    = db.Column(db.String(255))
+    part_number   = db.Column(db.String(255))
+    quantity      = db.Column(db.Float)
+    reference      = db.Column('Reference', db.Text) 
 
