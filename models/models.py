65e0f35a37de1340d614e0a44cf90504ea6ac0c5
playoung2818 <jameschen2818@gmail.com>
2025-07-22 11:57:03 -0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, String, Float, Date, Integer
 
 db = SQLAlchemy()
 
 class ReceivingLog(db.Model):
     __tablename__ = 'receiving_log'
+    id = Column(Integer, primary_key=True, autoincrement=True)
     serial_number = db.Column(db.String(255), primary_key=True)
     entry_date    = db.Column(db.Date)
     invoice_number= db.Column(db.String(255))
