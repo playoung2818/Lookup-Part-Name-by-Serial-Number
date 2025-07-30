2e55a03a7d39d8378ce96cdd7a887258e9d5aaef
playoung2818 <jameschen2818@gmail.com>
2025-07-30 14:04:05 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,5 @@
 import logging
 
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Czheyuan0227%40@localhost:5432/File_Log'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-#     LOG_LEVEL = logging.INFO
-
-
-
 class Config:
     SQLALCHEMY_DATABASE_URI = (
         "postgresql://postgres.avcznjglmqhmzqtsrlfg:"
